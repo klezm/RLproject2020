feb59beac19eeae59adec3afcf642fd7028b5185
Bretinator <56660188+Bretinator@users.noreply.github.com>
2021-06-16 18:25:38 +0200

    MyVar Basic functionality + some debug

--- MyVar.py
+++ MyVar.py
@@ -1,16 +1,19 @@
 import tkinter as tk
 from random import random
+import inspect
 # todo: alles tkVar.get() dann zu super().get()
 
 
 class MyVar:
     def __init__(self, value, *args, check_func=lambda arg: True, transform_func=lambda arg: arg, str_pre_transform_func=lambda arg: arg, **kwargs):
         self.value = None  # Will never be accessed for code use, but updated when trace is called. Useful fpr debugger, since its not able to show the native value in the tk.container
+        self.processingInit = True
         self.processingTrace = False
         self.processingSet = False
         #self.processingGet = False
         self.tkVar = tk.Variable()
-        self.trace_add(lambda: None)
+        self.tkVar.trace_add("write", self.traceFunc_wrapper)
+        self.custom_traces = []
         self.check_func = check_func
         self.transform_func = transform_func
         self.str_pre_transform_func = str_pre_transform_func
@@ -20,11 +23,31 @@ class MyVar:
         #self.transformedValue: object = None
         # the 4 declarations above will get a value assigned in the set() call below
         self.set(value)
+        self.processingInit = False
+
+    def traceFunc_wrapper(self, *traceArgs):
+        self.processingTrace = True  # activate this here since it should stay activated also during the callFunc call
+        if not self.processingSet:  # if self.processingSet would be true, the value is isValid and staySame warning have already resolved at this point and the value is good (white entry)
+            newValue = self.get()
+            # newValue = self.tkVar.get()
+            self.process_new_value(newValue)  # if get fails here, it will not throw an error but return None, which by definition wont pass the check
+            if not self.isValid:
+                print("Coloring all entries red")
+                self.processingTrace = False
+                return  # return immediately! # Ein leeres Entry zb darf eben nicht sofort wieder durch den letzten safe value ersetzt werden, erschwert die bedienung sehr. erst, wenn das value gebrauchtt wird (get call) soll es sichtbar ersetzt werden!"
+            if not self.staysSame:
+                print("Coloring all entries orange")
+                self.processingTrace = False
+                return  # return immediately!
+        print("Coloring all entries white")  # warum hier 2mal vorbei?
+        for func in self.custom_traces:
+            func(self)
+        self.processingTrace = False
 
     def process_new_value(self, value):
-        self.isValid = True
-        self.staysSame = True
-        tempValue = value
+        #self.isValid = True
+        #self.staysSame = True  # pretty sure this isnt needed anymore. staySame might seem not well defined if isValid is False, but staysSame will anyway only be read if isValid is True. Even if this line would be used, its unclear if assigning False is the best practice for upcoming changes of this class.
+        tempValue = value  # original arg should not be overwritten since it may be needed to perform the equality check to determine staysSame.
         try:
             if isinstance(value, str):
                 tempValue = self.str_pre_transform_func(tempValue)
@@ -42,30 +65,13 @@ class MyVar:
                 print(f"self.staysSame is now {self.staysSame}")  # warum hier 2mal vorbei?
             except:
                 print(f"Backwards cast failed. self.staysSame is still {self.staysSame}")
+        elif self.processingInit:
+            raise ValueError(f"{value} is no valid initialization value for {self.tkVar}.")
 
-    def trace_add(self, input_func, callFunc=False, passTraceArgs=False):
-        def traceFunc(*traceArgs):
-            self.processingTrace = True  # activate this here since it should stay activated also during the callFunc call
-            if not self.processingSet:  # if self.processingSet would be true, the value is isValid and staySame warning have already resolved at this point and the value is good (white entry)
-                newValue = self.get()
-                #newValue = self.tkVar.get()
-                self.process_new_value(newValue)  # if get fails here, it will not throw an error but return None, which by definition wont pass the check
-                if not self.isValid:
-                    print("Coloring all entries red")
-                    self.processingTrace = False
-                    return  # return immediately! # Ein leeres Entry zb darf eben nicht sofort wieder durch den letzten safe value ersetzt werden, erschwert die bedienung sehr. erst, wenn das value gebrauchtt wird (get call) soll es sichtbar ersetzt werden!"
-                if not self.staysSame:
-                    print("Coloring all entries orange")
-                    self.processingTrace = False
-                    return  # return immediately!
-            print("Coloring all entries white")  # warum hier 2mal vorbei?
-            if not passTraceArgs:
-                traceArgs = ()
-            input_func(*traceArgs)
-            self.processingTrace = False
-        self.tkVar.trace_add("write", traceFunc)
+    def trace_add(self, input_func, callFunc=False):
+        self.custom_traces.append(input_func)
         if callFunc:
-            traceFunc()
+            input_func(self)
 
     def set(self, value):
         self.process_new_value(value)
@@ -80,6 +86,8 @@ class MyVar:
         self.processingSet = True  # keep this here rather than in the first line to avoid the line "self.processingSet = False" an additional time before the return in the "if not self.isValid" case above
         self.tkVar.set(self.lastValidValue)  # calls trace!
         self.processingSet = False
+        self.staysSame = True
+        self.isValid = True
         self.value = self.lastValidValue
 
     def get(self):
@@ -124,8 +132,11 @@ if __name__ == "__main__":
         print(var.get())
 
 
-    def varTraceFunc():
-        print(f"Thats the tracefunc {var.get()}")
+    def varTraceFunc(varArg):
+        print(f"Thats the tracefunc!!!!!!!!!!!!!!!!!!!!!!!!!!!! {varArg.get()}")
+
+    def varTraceFunc2(varArg):
+        print(f"Thats the second of two tracefuncs!!!!!!!!!!!!!!!!!!!!!!!!!!!! {varArg.get()}")
 
 
     root = tk.Tk()
@@ -133,8 +144,9 @@ if __name__ == "__main__":
     # 2 entries die beide in der gleichen trace benutzt werden!
     # var = SafeVar(tk.IntVar, defaultValue=1)  # check initialize with default and different vatTypes
     # var = SafeVar(tk.Variable, defaultValue=4.6)  # check initialize with default and different vatTypes
-    var = MyVar(2, check_func=lambda i: 0 < i < 10, transform_func=float)#int, str_pre_transform_func=float)
+    var = MyVar(1.2, check_func=lambda i: 0 < i < 10, transform_func=int, str_pre_transform_func=float)
     var.trace_add(varTraceFunc, callFunc=True)
+    var.trace_add(varTraceFunc2, callFunc=True)
     entry = tk.Entry(root, textvariable=var.tkVar)
     entry.pack()
     tk.Button(root, text="get test", command=get_test).pack()
